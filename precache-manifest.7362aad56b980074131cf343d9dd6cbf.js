9e76637100866a3d018f127eb0429309508fd1e3
bhavesh-gulabani <bhaveshgulabani1@gmail.com>
2021-06-10 15:06:31 +0530

    Updates

---
+++ precache-manifest.7362aad56b980074131cf343d9dd6cbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffbb61c51e7c70f70965303136b4a634",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "b9239e29e1b6dca76c51",
+    "url": "/smart-brain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f0f3351dc4a22d9a9954",
+    "url": "/smart-brain/static/css/main.69b97432.chunk.css"
+  },
+  {
+    "revision": "b9239e29e1b6dca76c51",
+    "url": "/smart-brain/static/js/2.5681da2c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/smart-brain/static/js/2.5681da2c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f0f3351dc4a22d9a9954",
+    "url": "/smart-brain/static/js/main.412218c8.chunk.js"
+  },
+  {
+    "revision": "2beedbd042bf4b799852",
+    "url": "/smart-brain/static/js/runtime-main.f39d4b8b.js"
+  }
+]);
